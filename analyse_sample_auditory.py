288899f8e58137d2ce6c6e61b13b9160e3fb8bc4
Sara Somma <sommariva@dima.unige.it>
2019-09-13 14:07:38 +0200

    flake8 is happy with analyse_sample_auditory.py

--- analyse_sample_auditory.py
+++ analyse_sample_auditory.py
@@ -31,7 +31,7 @@ fname_fwd = op.join(data_path, 'MEG', subject,
                     'sample_audvis-meg-eeg-oct-6-fwd.fif')
 fname_evoked = op.join(data_path, 'MEG', subject, 'sample_audvis-ave.fif')
 fname_cov = op.join(sample.data_path(), 'MEG', subject,
-                             'sample_audvis-cov.fif')
+                    'sample_audvis-cov.fif')
 
 ###############################################################################
 # Load fwd model and evoked data
@@ -40,17 +40,19 @@ eeg_sensor_type = False
 
 # Fwd model
 fwd = read_forward_solution(fname_fwd, exclude='bads')
-fwd = pick_types_forward(fwd, meg=meg_sensor_type, eeg=eeg_sensor_type, ref_meg=False)
+fwd = pick_types_forward(fwd, meg=meg_sensor_type,
+                         eeg=eeg_sensor_type, ref_meg=False)
 
 # Evoked Data
 condition = 'Left Auditory'
 evoked = read_evokeds(fname_evoked, condition=condition, baseline=(None, 0))
-evoked = evoked.pick_types(meg=meg_sensor_type, eeg=eeg_sensor_type, exclude='bads')
+evoked = evoked.pick_types(meg=meg_sensor_type,
+                           eeg=eeg_sensor_type, exclude='bads')
 # TODO: there should be some check of consistency with fwd
 
 # Noise covariance (if provided data will be pre-whitened)
 cov = None
-#cov = read_cov(fname_cov)
+# cov = read_cov(fname_cov)
 
 ###############################################################################
 # Define SASMC parameters
@@ -76,8 +78,8 @@ sigma_noise = None
 sigma_q = None
 
 _sesame = Sesame(fwd, evoked, n_parts=n_parts, s_noise=sigma_noise,
-               sample_min=sample_min, sample_max=sample_max,
-               s_q=sigma_q, cov=cov, subsample=subsample, verbose=False)
+                 sample_min=sample_min, sample_max=sample_max,
+                 s_q=sigma_q, cov=cov, subsample=subsample, verbose=False)
 _sesame.apply_sesame()
 
 print('    Estimated number of sources: {0}'.format(_sesame.est_n_dips[-1]))
@@ -111,8 +113,8 @@ for idx, loc in enumerate(est_locs):
         brain.add_foci(stc.vertices[0][loc], coords_as_verts=True,
                        hemi='lh', color=colors[idx], scale_factor=0.3)
     else:
-       brain.add_foci(stc.vertices[1][loc-nv_lh], coords_as_verts=True,
-                      hemi='rh', color=colors[idx], scale_factor=0.3)
+        brain.add_foci(stc.vertices[1][loc-nv_lh], coords_as_verts=True,
+                       hemi='rh', color=colors[idx], scale_factor=0.3)
 mlab.show()
 
 ###############################################################################
@@ -124,17 +126,21 @@ times = np.tile(evoked.times[_sesame.s_min:_sesame.s_max+1], (2, 1))
 pos = _sesame.source_space[_sesame.est_locs[-1]]
 num_dip = pos.shape[0]
 amplitude = np.array([
-      np.linalg.norm(_sesame.est_q[:, i_dip:3*(i_dip+1)], axis=1) for i_dip in range(num_dip)])
+                      np.linalg.norm(_sesame.est_q[:, i_dip:3*(i_dip+1)],
+                                     axis=1) for i_dip in range(num_dip)])
 # TODO: Understand/compute orientation and gof (xfit documentation)
 #       One possibility: pick and plot one selected time-point.
 orientation = np.array([np.array([0, 0, 1]) for i_dip in range(num_dip)])
 gof = np.array([0 for i_dip in range(num_dip)])
 
-dips = mneDipole(np.tile(times[0], num_dip), pos, amplitude[:, 0], orientation, gof)
-fname_trans = op.join(data_path, 'MEG', 'sample', 'sample_audvis_raw-trans.fif')
+dips = mneDipole(np.tile(times[0], num_dip), pos,
+                 amplitude[:, 0], orientation, gof)
+fname_trans = op.join(data_path, 'MEG', 'sample',
+                      'sample_audvis_raw-trans.fif')
 
 for i_dip in range(num_dip):
-    dips.plot_locations(fname_trans, 'sample', subjects_dir, mode='orthoview', idx=i_dip)
+    dips.plot_locations(fname_trans, 'sample',
+                        subjects_dir, mode='orthoview', idx=i_dip)
 
 
 plt.show()
@@ -147,8 +153,8 @@ plt.show()
 #     for j in range(est_cs.shape[0]):
 #         est_q[j, i, :] += _temp[j]
 #
-# fname_trans = op.join(data_path, 'MEG', 'sample', 'sample_audvis_raw-trans.fif')
+# fname_trans = op.join(data_path, 'MEG',
+#                       'sample', 'sample_audvis_raw-trans.fif')
 # # TODO: How to properly deal with time-point selection?
-# # TODO: How is related the time interval in dipole-fitting tutorial with N100????
 # mlab.show()
 # Motivation for control on integer
